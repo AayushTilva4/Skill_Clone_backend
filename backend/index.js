0a9601ea74a12d318ec194184d70c5efcff3e04d
AayushTilva4 <159879461+AayushTilva4@users.noreply.github.com>
2025-07-20 14:36:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 import app from './src/utils/app.js';
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000} at ${new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}`);
+// app.listen(process.env.PORT || 3000, () => {
+//   console.log(`Server running on port ${process.env.PORT || 3000} at ${new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}`);
+// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
